ac9ddb5bf537fb35edc02b360bb8b4246b68894b
ambrojessica <ambrojessica0@gmail.com>
2021-12-15 20:11:56 -0600

    almost done, i think

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import MovieHeader from './components/MovieHeader';
 
 import FavoriteMovieList from './components/FavoriteMovieList';
 import EditMovieForm from './components/EditMovieForm';
+import AddMovieForm from "./components/AddMovieForm";
 
 import axios from 'axios';
 
